3c095cd347ddc693fa397f753e7592837482b8dd
nicolas <nicroraza@gmail.com>
2025-06-10 20:28:07 +0300

    tailwind update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./src/html/**/*.html",
-    "./src/html/**/*.js",
-    "./src/javascript/**/*.js"
-  ],
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      colors:{
-        mainblue: '#1B3C5F',      // Darker navy blue - for header and primary elements
-        secondaryblue: '#4A6FA5', // Softer medium blue - for interactive elements
-        bgblue: '#F5F7FA',  
+      colors: {
+        mainblue: '#1B3C5F',
+        secondaryblue: '#4A6FA5',
+        bgblue: '#F5F7FA',
       }
     },
   },
