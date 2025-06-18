26b365799a45b440e9f9c36ddf5a92b25cd6b7fd
nicolas <nicroraza@gmail.com>
2025-06-18 22:36:21 +0300

    test6.1

--- src/javascript/utils/wapp.js
+++ src/javascript/utils/wapp.js
@@ -1,4 +1,4 @@
-fetch('/src/html/utils/wapp.html')
+fetch('/artools-v2/src/html/utils/wapp.html')
   .then(response => response.text())
   .then(data => {
     document.querySelector('.wapp').innerHTML = data;
