cf71ab601e1f4418dfc35ace3580ce642a609543
nicolas <nicroraza@gmail.com>
2025-06-18 22:40:48 +0300

    test6.2

--- src/javascript/utils/wapp.js
+++ src/javascript/utils/wapp.js
@@ -1,4 +1,4 @@
-fetch('/artools-v2/src/html/utils/wapp.html')
+fetch('/src/html/utils/wapp.html')
   .then(response => response.text())
   .then(data => {
     document.querySelector('.wapp').innerHTML = data;
