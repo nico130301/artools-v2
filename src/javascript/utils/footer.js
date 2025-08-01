175878ef1f8f8621d1ee102b61637959829b67b6
nicolas <nicroraza@gmail.com>
2025-08-01 18:29:59 +0300

    .

--- src/javascript/utils/footer.js
+++ src/javascript/utils/footer.js
@@ -101,7 +101,7 @@ document.addEventListener('DOMContentLoaded', async () => {
           const email = unsubscribeForm.email.value;
 
           try {
-            const response = await fetch('https://formsubmit.co/ajax/contact@artoolsweb.com', {
+            const response = await fetch('https://formsubmit.co/ajax/info@artoolsweb.com', {
               method: 'POST',
               headers: {
                 'Content-Type': 'application/json',
